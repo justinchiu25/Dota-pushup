96db396e5bc03cb434a62b6fbd7d7cb5dbee8105
justinchiu25 <justinchiu35@gmail.com>
2022-06-01 05:04:44 -0400

    Initial Commit

---
+++ src/App/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+
+export default function Navbar() {
+
+    return (
+        <>
+            <div> Navbar </div>
+
+            <Outlet /> {/*Outlet important to render children*/} 
+        </>
+    )
+}
