2a285983f5afb6f51926cb734580ba4d43e6a3b2
justinchiu25 <justinchiu35@gmail.com>
2022-06-18 04:09:00 -0400

    Correctly updates database on refresh

--- src/App/Components/Profile.js
+++ src/App/Components/Profile.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUser, updateDeaths } from "../Redux/User";
+import { fetchUser, updateUser } from "../Redux/User";
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import axios from "axios";
@@ -12,22 +12,22 @@ export default function Profile() {
     const user = useSelector((state) => state.user);
     const { userId } = useParams()
     const dispatch = useDispatch();
-    const { data, error} = useSWR(`https://api.opendota.com/api/players/${userId}/recentMatches`, fetcher);
+    const { data } = useSWR(`https://api.opendota.com/api/players/${userId}/recentMatches`, fetcher);
 
     useEffect( () => {
         dispatch(fetchUser(userId)) //current page id
-    },[userId])
+    },[userId, dispatch])
     
     function updateInfo() {
         console.log("updating"); 
         if(Object.keys(user).length > 0) {
-            dispatch(updateDeaths(user, data))
+            dispatch(updateUser(user, data))
         }
     }
 
     return (
         <>
-            <img src={user.profileImage} />
+            <img src={user.profileImage} alt="profile" />
             <div> {user.gameName} has to do {user.pushUp} push ups.</div>
             <button onClick={updateInfo}> Refresh stats </button>
         </>
