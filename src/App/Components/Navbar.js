05f8411dbe4380e03109c6686194e6f299118f93
justinchiu25 <justinchiu35@gmail.com>
2022-06-18 22:24:48 -0400

    Logout feature & console.logs

--- src/App/Components/Navbar.js
+++ src/App/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 import { Nav, Navbar as Navbarb, NavDropdown } from "react-bootstrap";
 import { useAuth } from "../Contexts/AuthContext";
@@ -9,13 +9,24 @@ import { setAuth } from "../Redux/Auth";
 export default function Navbar() {
     const dispatch = useDispatch();
     const authUser = useSelector((state) => state.auth);
-    const { currentUser } = useAuth();
+    const { currentUser, logout } = useAuth();
+    const navigate = useNavigate();
+
+    async function handleLogout(evt) {
+        evt.preventDefault();
+        try {
+            await logout();
+            navigate("/login");
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     useEffect( () => {
         if (currentUser){
             dispatch(setAuth(currentUser));
         }
-    }, [currentUser])
+    }, [currentUser, dispatch])
     
     return (
         <>
@@ -32,7 +43,7 @@ export default function Navbar() {
                         <Nav.Link as={Link} to={"/claim"}> Profile </Nav.Link>}
                         <NavDropdown title="Settings">
                             <NavDropdown.Item href="/"> Edit Profile </NavDropdown.Item>
-                            <NavDropdown.Item href="/"> Log Out </NavDropdown.Item>
+                            <NavDropdown.Item onClick={handleLogout} href="/"> Log Out </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbarb>
--- src/App/Contexts/AuthContext.js
+++ src/App/Contexts/AuthContext.js
@@ -20,6 +20,11 @@ export function AuthProvider({children}) {
         return auth.signInWithEmailAndPassword(email,password);
     }
 
+    function logout() {
+        setCurrentUser(null);
+        return auth.signOut();
+    }
+
     useEffect( () => {
         const unsubscribe = auth.onAuthStateChanged( (user) => {
             setCurrentUser(user);
@@ -32,7 +37,8 @@ export function AuthProvider({children}) {
     const value = {
         currentUser,
         signUp,
-        login
+        login,
+        logout
     }
 
     return (
