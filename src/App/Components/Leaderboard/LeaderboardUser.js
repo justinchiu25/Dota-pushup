7b2e26545630c53ca3accf358da7f880578bbdd8
justinchiu25 <justinchiu35@gmail.com>
2022-06-21 16:09:54 -0400

    Added profile image to leaderboard

--- src/App/Components/Leaderboard/LeaderboardUser.js
+++ src/App/Components/Leaderboard/LeaderboardUser.js
@@ -5,7 +5,11 @@ export default function LeaderboardUser( { user, index } ) {
     return (
         <tr>
             <th scope="row"> {index+1} </th>
-            <td> {user.gameName} </td>
+            <td>
+                <img src={user.profileImage} style={{maxHeight: "50px", maxWidth: "50px"}}></img>
+                <> </>
+                {user.gameName} 
+            </td>
             <td> {user.pushUp} </td>
             <td> {user.completed} </td>
         </tr>
