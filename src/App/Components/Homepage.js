b7229fcc91d187230489362cf8fbc9c2dbf6f632
justinchiu25 <justinchiu35@gmail.com>
2022-06-23 15:41:25 -0400

    Moved files to backend - updates daily

--- src/App/Components/Homepage.js
+++ src/App/Components/Homepage.js
@@ -1,16 +1,9 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { pollingAction } from "../Redux/Polling";
 
 export default function Homepage() {
-    const dispatch = useDispatch();
-
-    function handleClick(evt) {
-        evt.preventDefault();
-        dispatch(pollingAction());
-    }
 
     return (
-        <button type="button" onClick={handleClick}> button </button>
+        <div> This is the homepage! WOW! </div>
     )
 }
--- src/App/Store.js
+++ src/App/Store.js
@@ -5,13 +5,11 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import userReducer from "./Redux/User";
 import authReducer from "./Redux/Auth";
 import leaderboardReducer from "./Redux/Leaderboard";
-import pollingReducer from "./Redux/Polling";
 
 const reducer = combineReducers({
   user: userReducer,
   auth: authReducer,
-  leaderboard: leaderboardReducer,
-  polling: pollingReducer
+  leaderboard: leaderboardReducer
 });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
