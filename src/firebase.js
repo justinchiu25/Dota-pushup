ac6b3f91103a22438a5ab2155791d3c03bea32d7
justinchiu25 <justinchiu35@gmail.com>
2022-06-23 22:44:07 -0400

    change firebasekey

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import firebasekey from "./firebaseKey.json"
+const firebasekey = require("./firebaseKey.json")
 
 //MUST CHANGE THIS TO PROCESS.ENV
 const firebaseConfig = {
