e3a4258c034745f0e7b2ea63ddba03c2ec186332
justinchiu25 <justinchiu35@gmail.com>
2022-06-27 15:40:29 -0400

    dark background

--- src/App/Components/Homepage.js
+++ src/App/Components/Homepage.js
@@ -3,6 +3,9 @@ import React from "react";
 export default function Homepage() {
 
     return (
-        <div> This is the homepage! WOW! Yep </div>
+        <div className="bg-dark" style={{height: "100vh", width: "100vw"}}>
+            <div className="text-white"> This is the homepage! WOW! Yep </div>
+
+        </div>
     )
 }
