5ebf77a881b2590ec968c61f32c37e822b419238
justinchiu25 <justinchiu35@gmail.com>
2022-06-28 16:58:37 -0400

    Added image to homepage

--- src/App/Components/Homepage.js
+++ src/App/Components/Homepage.js
@@ -4,8 +4,11 @@ export default function Homepage() {
 
     return (
         <div className="bg-dark" style={{height: "100vh", width: "100vw"}}>
-            <div className="text-white"> This is the homepage! WOW! Yep </div>
-
+            <div className="text-white" style={{textAlign: "center", height: "80vh"}} >
+                <img src="/dawnbreaker.jpg" alt="dawnbreaker"></img>
+                <h1> Push Up </h1>
+                <div> Push up tracker for Dota 2 deaths </div>
+            </div>
         </div>
     )
 }
